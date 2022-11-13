bee619286a3d13a5477b751e3346825e75cd31fb
aro <pantoine.rochas@gmail.com>
2022-11-13 16:56:35 +0100

    feat: front only, buttons to stop and add

--- src/main/kotlin/xyz/atnrch/wrench/display/WrenchDisplay.kt
+++ src/main/kotlin/xyz/atnrch/wrench/display/WrenchDisplay.kt
@@ -1,13 +1,12 @@
 package xyz.atnrch.wrench.display
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WrenchDisplay(
@@ -18,8 +17,17 @@ fun WrenchDisplay(
         modifier = Modifier.fillMaxSize()
     ) {
         Row {
+
             Button(onStartButtonClick) {
                 Text("Watch")
+            }
+            Spacer(Modifier.width(16.dp))
+            Button({}) {
+                Text("Stop")
+            }
+            Spacer(Modifier.width(16.dp))
+            Button({}) {
+                Text("Add...")
             }
         }
     }
