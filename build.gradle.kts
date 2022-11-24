ca2b9c634478e1c4e1a3e4e7fd81ddd306297176
aro <pantoine.rochas@gmail.com>
2022-11-24 13:27:39 +0100

    build: extended material icons

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
-import org.jetbrains.compose.compose
+
+import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.1"
+    kotlin("jvm") version "1.7.20"
+    id("org.jetbrains.compose") version "1.2.1"
 }
 
 group = "xyz.atnrch"
@@ -16,8 +17,11 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
+@OptIn(ExperimentalComposeLibrary::class)
 dependencies {
     implementation(compose.desktop.currentOs)
+    implementation(compose.materialIconsExtended)
+    implementation(compose.material3)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.6.4")
 }
 
