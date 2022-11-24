02043a325ee9a844b4a99d3353a5a51bbd910d90
aro <pantoine.rochas@gmail.com>
2022-11-24 13:31:14 +0100

    app style: colors/icon

--- src/main/kotlin/xyz/atnrch/wrench/components/AddButton.kt
+++ src/main/kotlin/xyz/atnrch/wrench/components/AddButton.kt
@@ -4,9 +4,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.rounded.PostAdd
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import xyz.atnrch.wrench.logger.Logger
 import xyz.atnrch.wrench.ui.UIColors
@@ -28,11 +29,11 @@ fun AddButton(
                 Logger.info("No file selected.")
             })
         },
-        backgroundColor = UIColors.LIGHT,
+        backgroundColor = UIColors.PRIMARY,
     ) {
         Icon(
-            Icons.Filled.Add,
-            tint = UIColors.GREEN,
+            Icons.Rounded.PostAdd,
+            tint = Color.White,
             contentDescription = "Add file...",
             modifier = Modifier.size(28.dp)
         )
