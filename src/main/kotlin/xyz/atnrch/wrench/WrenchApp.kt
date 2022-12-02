379d1b2f2e5596e03d647e710766f84305495051
aro <pantoine.rochas@gmail.com>
2022-12-02 17:19:45 +0100

    refactor: simpler entrypoint

--- src/main/kotlin/xyz/atnrch/wrench/WrenchApp.kt
+++ src/main/kotlin/xyz/atnrch/wrench/WrenchApp.kt
@@ -1,12 +1,9 @@
 package xyz.atnrch.wrench
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import xyz.atnrch.wrench.logger.Logger
+import androidx.compose.ui.window.singleWindowApplication
 import xyz.atnrch.wrench.components.WrenchScaffold
+import xyz.atnrch.wrench.logger.Logger
 import javax.swing.UIManager
 
 class WrenchApp {
@@ -14,23 +11,8 @@ class WrenchApp {
         const val PREFIX: String = "(Wrench)"
 
         @JvmStatic
-        fun main(args: Array<String>) {
-            application {
-                Window(onCloseRequest = {
-                    Logger.info("Stopping Wrench...")
-                    Logger.info("bye!")
-                    ::exitApplication.invoke()
-                }, title = "Wrench") {
-                    Logger.info("Starting Wrench...")
-                    App()
-                }
-            }
-        }
-
-        @Composable
-        @Preview
-        @JvmStatic
-        fun App() {
+        fun main(args: Array<String>) = singleWindowApplication {
+            Logger.info("Starting Wrench...")
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
             MaterialTheme { WrenchScaffold() }
         }
---
+++ src/main/kotlin/xyz/atnrch/wrench/components/bottom/RemoveOutputButton.kt
@@ -0,0 +1,5 @@
+package xyz.atnrch.wrench.components.bottom
+
+class RemoveOutputButton(watcherManager: WatcherManager, outputs: MutableList<Path>, currentClick: Int) {
+
+}
